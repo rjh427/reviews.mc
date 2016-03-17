c7e4d87952e518a9f4be0787e08392afb254c346
Reese Howell <reese.howell@dominionenterprises.com>
2016-03-17 10:06:21 -0400

    corrections to rich snippets declarations

--- single-post.php
+++ single-post.php
@@ -1,157 +1,156 @@
 <?php
 /**
- * A post template for displaying m/c reviews
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @subsubpackate Reviews-Child, a child-theme of Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
+* A post template for displaying m/c reviews
+*
+* @package WordPress
+* @subpackage Twenty_Fifteen
+* @subsubpackate Reviews-Child, a child-theme of Twenty_Fifteen
+* @since Twenty Fifteen 1.0
+*/
 /*
-    echo 'is_front_page: ', var_dump(is_front_page()), PHP_EOL;
-    echo 'is_home: ', var_dump(is_home()), PHP_EOL;
-    echo 'is_single: ', var_dump(is_single()), PHP_EOL;
-    echo 'is_page: ', var_dump(is_page()), PHP_EOL;
-    echo 'is_page_template: ', var_dump(is_page_template()), PHP_EOL;
-    echo 'is_search: ', var_dump(is_search()), PHP_EOL;
-    echo 'is_singular: ', var_dump(is_singular()), PHP_EOL;
-    echo 'in_the_loop: ', var_dump(in_the_loop()), PHP_EOL;
+echo 'is_front_page: ', var_dump(is_front_page()), PHP_EOL;
+echo 'is_home: ', var_dump(is_home()), PHP_EOL;
+echo 'is_single: ', var_dump(is_single()), PHP_EOL;
+echo 'is_page: ', var_dump(is_page()), PHP_EOL;
+echo 'is_page_template: ', var_dump(is_page_template()), PHP_EOL;
+echo 'is_search: ', var_dump(is_search()), PHP_EOL;
+echo 'is_singular: ', var_dump(is_singular()), PHP_EOL;
+echo 'in_the_loop: ', var_dump(in_the_loop()), PHP_EOL;
 */
 
 get_header();?>
 
 <div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+<main id="main" class="site-main" role="main">
 
 <?php
 // Start the loop.
 while ( have_posts() ) : the_post();
-	/*
-	 * Include the post format-specific template for the content. If you want to
-	 * use this in a child theme, then include a file called called content-___.php
-	 * (where ___ is the post format) and that will be used instead.
-	 */
+/*
+* Include the post format-specific template for the content. If you want to
+* use this in a child theme, then include a file called called content-___.php
+* (where ___ is the post format) and that will be used instead.
+*/
 //	get_template_part( 'content', get_post_format() );
 
 
-    $review = '';
+$review = '';
 // see output of var_dump($post) at bottom
-    global $post;
-    $review = '';
-    $upgrades = '';
-    $image = '';
-    $height = '';
-    $weight = '';
-    $milesHours = '';
-    $location = '';
-    $ctol_url = '';
-    $imageSubmitted = '';
+global $post;
+$review = '';
+$upgrades = '';
+$image = '';
+$height = '';
+$weight = '';
+$milesHours = '';
+$location = '';
+$ctol_url = '';
+$imageSubmitted = '';
 
-    $postTitle = substr(get_the_title(), 0, strrpos(get_the_title(), ' '));
-    $reviewTitle =  post_custom('review_title');
-    
-    $make = htmlentities(post_custom('manufacturer'));
-    $model = htmlentities(post_custom('model'));
-    $overall = getRating('overall');
-    $overallRaw = getRatingRaw('overall');
-    $reliability = getRating('reliability');
-    $quality = getRating('quality');
-    $performance = getRating('performance');
-    $comfort = getRating('comfort');
+$postTitle = substr(get_the_title(), 0, strrpos(get_the_title(), ' '));
+$reviewTitle =  post_custom('review_title');
 
-    // for PSN photos on our CDN only, WP handles uploaded photos natively
-    if (post_custom('user_picture_filename') != '') {
-        $imageSubmitted = '<small class="review_date">Submitted ' . get_the_date('F Y') . ':</small><br />';
-        $image = '<img src="' . CDN_URL . post_custom('user_picture_filename') . '" alt="User-submitted image" class="userPict" /><br /><br >';
-    }
+$make = htmlentities(post_custom('manufacturer'));
+$model = htmlentities(post_custom('model'));
+$overall = getRating('overall');
+$overallRaw = getRatingRaw('overall');
+$reliability = getRating('reliability');
+$quality = getRating('quality');
+$performance = getRating('performance');
+$comfort = getRating('comfort');
 
-    if (post_custom('upgrades') != null) {
-        $upgrades = '<p>Upgrades:<br />' . post_custom('upgrades') . '</p>';
-    }
+// for PSN photos on our CDN only, WP handles uploaded photos natively
+if (post_custom('user_picture_filename') != '') {
+$imageSubmitted = '<small class="review_date">Submitted ' . get_the_date('F Y') . ':</small><br />';
+$image = '<img src="' . CDN_URL . post_custom('user_picture_filename') . '" alt="User-submitted image" class="userPict" /><br /><br >';
+}
 
-    if (post_custom('anon') == 'N' || post_custom('anon') != '') {
-        $author = '<td>Author:</td><td itemprop="author">' . post_custom('reviewer_name') . '</td>';
-    } else {
-        $author = '<td> Author:</td><td itemprop="author">Anonymous</td>';
-    }
+if (post_custom('upgrades') != null) {
+$upgrades = '<p>Upgrades:<br />' . post_custom('upgrades') . '</p>';
+}
 
-    if (post_custom('height') != null) {
-        $height = '<td>Height:</td><td>' . post_custom('height') . '</td>';
-    }
+if (post_custom('anon') == 'N' || post_custom('anon') != '') {
+$author = '<td>Author:</td><td itemprop="author">' . post_custom('reviewer_name') . '</td>';
+} else {
+$author = '<td> Author:</td><td itemprop="author">Anonymous</td>';
+}
 
-    if (post_custom('weight') != null) {
-        $weight = '<td>Weight:</td><td>' . post_custom('weight') . '</td>';
-    }
+if (post_custom('height') != null) {
+$height = '<td>Height:</td><td>' . post_custom('height') . '</td>';
+}
 
-    if (post_custom('miles_or_hours') != null) {
-        $milesHours = '<td>Miles or hours spent on the review:</td><td>' . post_custom('miles_or_hours') . '</td>';
-    }
+if (post_custom('weight') != null) {
+$weight = '<td>Weight:</td><td>' . post_custom('weight') . '</td>';
+}
 
-    if (post_custom('location') != null) {
-       $location = '<td>Location</td><td>' . post_custom('location') . '</td>';
-    }
+if (post_custom('miles_or_hours') != null) {
+$milesHours = '<td>Miles or hours spent on the review:</td><td>' . post_custom('miles_or_hours') . '</td>';
+}
 
-    // http://www.cycletrader.com/search-results?make=<url-safe-make-name>&model=<url-safe-model-name>&modelkeyword=1
-    // @TODO: move this into a URL-builder function
-    $ctol_url = 'View ' . '<a href="http://www.cycletrader.com/search-results?make=' . $make . '&model=' .
-        $model . '&modelkeyword=1">' . $make . ' '. $model . '</a> Motorcycles For Sale on ' .
-        '<a href="http://www.cycletrader.com">CycleTrader.com</a><br />';
+if (post_custom('location') != null) {
+$location = '<td>Location</td><td>' . post_custom('location') . '</td>';
+}
 
-    if (post_custom('user_picture_filename') == '') {
-        $dateSubmitted = '<small class="review_date">Submitted: ' . get_the_date('F, Y') . '</small>';
-    } else {
-        $dateSubmitted = '';
-    }
+// http://www.cycletrader.com/search-results?make=<url-safe-make-name>&model=<url-safe-model-name>&modelkeyword=1
+// @TODO: move this into a URL-builder function
+$ctol_url = 'View ' . '<a href="http://www.cycletrader.com/search-results?make=' . $make . '&model=' .
+$model . '&modelkeyword=1">' . $make . ' '. $model . '</a> Motorcycles For Sale on ' .
+'<a href="http://www.cycletrader.com">CycleTrader.com</a><br />';
 
-    $review = <<<EOT
+if (post_custom('user_picture_filename') == '') {
+$dateSubmitted = '<small class="review_date">Submitted: ' . get_the_date('F, Y') . '</small>';
+} else {
+$dateSubmitted = '';
+}
 
-    <div class="entry-content hentry" itemscope itemtype="http://schema.org/Review">
-        <div itemprop="itemReviewed" itemscope itemtype="http://schema.org/motorcycle">
-            <header>
-                <h1 class="entry-title" itemprop="name"> $postTitle Review</h1>
-                Review Title: <strong><span itemprop="name"> $reviewTitle </span></strong>
-            </header>
+$review = <<<EOT
+<div class="entry-content hentry" itemscope itemtype="http://schema.org/Review">
+    <header>
+        <h1 itemprop="itemReviewed" itemscope itemtype="http://schema.org/Motorcycle" class="entry-title" itemprop="name"> $postTitle Review</h1>
+        Review Title: <strong><span itemprop="name"> $reviewTitle </span></strong>
+    </header>
 
-            <table>
-              <tbody>
-                <tr><th colspan="2">Ratings</th></tr>
-                <tr> $overall </tr>
-                <tr colspan="2" class="hidden">
-                    <span class="hidden" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
-                        <span itemprop="ratingValue"> $overallRaw stars</span>
-                    </span>
-                </tr>
-                <tr> $reliability </tr>
-                <tr> $quality </tr>
-                <tr> $performance </tr>
-                <tr> $comfort </tr>
-              </tbody>
-            </table>
+    <table>
+        <tbody>
+            <tr><th colspan="2">Ratings</th></tr>
+            <tr> $overall </tr>
+            <tr colspan="2" class="hidden">
+                <span class="hidden" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+                    <span itemprop="ratingValue"> $overallRaw stars</span>
+                </span>
+            </tr>
+            <tr> $reliability </tr>
+            <tr> $quality </tr>
+            <tr> $performance </tr>
+            <tr> $comfort </tr>
+        </tbody>
+    </table>
 
-            <span itemprop="reviewBody"> $post->post_content </span>
+    <span itemprop="reviewBody"> $post->post_content </span>
 
-            $imageSubmitted
-            $image
-            $upgrades
+    $imageSubmitted
+    $image
+    $upgrades
 
-            <table><tr><th colspan="2">About the reviewer:</th></tr>
-                <tr> $author  </tr>
-                <tr> $height  </tr>
-                <tr> $weight </tr>
-                <tr> $milesHours </tr>
-                <tr> $location </tr>
-            </table>
+    <table>
+        <tbody>
+            <tr><th colspan="2">About the reviewer:</th></tr>
+            <tr> $author  </tr>
+            <tr> $height  </tr>
+            <tr> $weight </tr>
+            <tr> $milesHours </tr>
+            <tr> $location </tr>
+        </tbody>
+    </table>
 
-            $ctol_url
-            $dateSubmitted
+    $ctol_url
+    $dateSubmitted
 
-        </div><!-- itemtype -->
-    </div> <!-- .entry-content itemReviewed -->
+</div> <!-- .entry-content itemReviewed -->
 
 EOT;
 
     echo $review;
-
 
 	// Previous/next post navigation.
 	the_post_navigation( array(
